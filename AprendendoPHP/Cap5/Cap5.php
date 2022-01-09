7eaf5fabcec8288babffd017c4e0c03bb21da0a1
Jonatha Araujo Oliveira <55407633+jonathaemulador@users.noreply.github.com>
2022-01-09 13:11:14 -0300

    Create Cap5.php

---
+++ AprendendoPHP/Cap5/Cap5.php
@@ -0,0 +1,11 @@
+<?php
+
+#Grupos de lógica: funções e arquivos
+
+// declarando uma função
+
+function minhaFuncao() {
+    print "Estou imprimindo a função: minhaFuncao()";
+}
+
+echo (minhaFuncao());

192377ed34e94dbc6923f9ffbe74d1b2f62a0a82
Jonatha Araujo Oliveira <55407633+jonathaemulador@users.noreply.github.com>
2022-01-10 23:29:22 -0300

    Create Cap6.php

---
+++ AprendendoPHP/Cap6/Cap6.php
@@ -0,0 +1,12 @@
+<?php
+
+#Dados e lógica juntos : trabalhando com objetos
+
+// Classe          -> (Descreve variaveis e funções de um tipo de objeto);
+// Método          -> (Função definida em uma classe);
+// Propriedade     -> (Variavel definida em uma classe);
+// Instalancia     -> (Uso individual de uma classe, "instanciar um objeto");
+// Construtor      -> (Método especial, define as propriedades do objeto e deixa-o pronto para uso);
+// Método estático -> (Método especial, pode ser chamado sem instanciar de uma classe);
+
+
